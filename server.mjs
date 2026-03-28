38966afa5d1ff27c4f24cf672b384299ebcbd05c
gustavoaraujo2992-creator <gustavoaraujo2992@gmail.com>
2026-03-28 10:20:21 -0300

    2.2

--- server.mjs
+++ server.mjs
@@ -5,6 +5,7 @@ import Tesseract from 'tesseract.js';
 import cors from 'cors';
 import path from 'path';
 import { fileURLToPath } from 'url';
+import { createRequire } from 'module';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
